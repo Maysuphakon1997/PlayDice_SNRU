f7655c901074028bb6df8dafa02dbdc6dd43a8a9
doramon <doramon@abc.com>
2019-03-19 11:45:25 +0700

    Got

--- PlayDice/ViewController.swift
+++ PlayDice/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstDicelmageView: UIImageView!
+    
+    @IBOutlet weak var secondDicelmageView: UIImageView!
+    
+    @IBOutlet weak var thirdDicelmageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }  //Main Method
 
+    
+   
+    
+    @IBAction func ramdomDiceButton(_ sender: UIButton) {
+        
+        
+ print("You Click Ramdom")
+        
+        
+    } // ramdonmDice
+    
+    
 
-}
+}  //Main Class
 
